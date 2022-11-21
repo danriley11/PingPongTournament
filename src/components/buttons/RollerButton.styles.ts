b547a8a1fd53bb151d67c398f85d917a85ea7417
danriley11 <dan_riley7@outlook.com>
2022-11-21 18:18:09 +1100

    base components

---
+++ src/components/buttons/RollerButton.styles.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const ButtonContainer = styled.button.attrs({
+  type: "button",
+})`
+  background-color: grey;
+  font-size: large;
+  align-self: center;
+`;
