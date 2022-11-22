af228dc5c1f6422258c5183550b74bbe5b38b36a
danriley11 <dan_riley7@outlook.com>
2022-11-23 08:28:54 +1100

    resolve errors

--- src/components/inputs/LabelAndNumberInput.tsx
+++ src/components/inputs/LabelAndNumberInput.tsx
@@ -1,16 +1,25 @@
-import { InputLabel, LabelAndInputContainer, TextInput } from "./LabelAndTextInput.styles";
+import { Controller, useForm } from 'react-hook-form';
+import { InputLabel, LabelAndInputContainer, TextInput } from './LabelAndTextInput.styles';
 
 type LabelAndNumberInputProps = {
   label: string;
   value: number;
 };
 
-// monetaryInput
-const LabelAndNumberInput = ({ label, value }: LabelAndNumberInputProps) => (
-  <LabelAndInputContainer>
-    <InputLabel>{label}</InputLabel>
-    <TextInput value={value | 0} />
-  </LabelAndInputContainer>
-);
+const LabelAndNumberInput = ({ label, value }: LabelAndNumberInputProps) => {
+  const { control } = useForm();
 
+  return (
+    <Controller
+      control={control}
+      name={label}
+      render={({ field }) => (
+        <LabelAndInputContainer>
+          <InputLabel>{label}</InputLabel>
+          <TextInput value={field.value || 0} onChange={field.onChange} />
+        </LabelAndInputContainer>
+      )}
+    />
+  );
+};
 export default LabelAndNumberInput;
